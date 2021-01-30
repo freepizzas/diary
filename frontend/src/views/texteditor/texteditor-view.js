a4f7971c7fb42e761aedcbcc5c534b4ef2fb32e2
starcictoni <57995236+starcictoni@users.noreply.github.com>
2021-01-30 17:25:36 +0100

    added rt button

--- frontend/src/views/texteditor/texteditor-view.js
+++ frontend/src/views/texteditor/texteditor-view.js
@@ -2,22 +2,26 @@ import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-vertical-layout.js';
 import '@vaadin/vaadin-rich-text-editor/src/vaadin-rich-text-editor.js';
 import '@vaadin/vaadin-ordered-layout/src/vaadin-horizontal-layout.js';
+import '@vaadin/vaadin-button/src/vaadin-button.js';
 
 class TexteditorView extends PolymerElement {
 
     static get template() {
         return html`
-            <style include="shared-styles">
+<style include="shared-styles">
                 :host {
                     display: block;
                     width: 100%;
                     height: 100%;
                 }
 
-                .background {
-                    background-color: rosybrown;
+                /*.background {*/
+                /*    background-color: rosybrown;*/
+                /*}*/
+                /*vaadin-rich*/
+                [part="toolbar"] {
+                    background-color: black;
                 }
-
                 .editor {
                     padding-right: 5%;
                     padding-left: 5%;
@@ -25,16 +29,35 @@ class TexteditorView extends PolymerElement {
                     width: 100%;
                     height: 100%;
                 }
+                .btn {
+                    margin-left: auto;
+                    margin-right: 5%;
+                    margin-top: -4.5%;
+                    font-weight: 600;
+                    background-color: #ff4f5a;
+                    color: white;
+                    display: block;
+                    text-align: center;
+                    text-decoration: none;
+                    font-size: 20px;
+                    border-radius: 25px;
+                    border: 1px solid white;
+                    line-height: 1;
+                    height: 35px;
+                }
             </style>
-            <vaadin-vertical-layout style="width: 100%; height: 100%;">
-                <vaadin-horizontal-layout style="width:100%; height: 20%" class="background">
-                    <!-- Tu ide mood ili ask me a question-->
-                </vaadin-horizontal-layout>
-                <vaadin-vertical-layout style="height: 100%; width:100%;" class="background">
-                    <vaadin-rich-text-editor class="editor" html-value="<p><br></p>"></vaadin-rich-text-editor>
-                </vaadin-vertical-layout>
-            </vaadin-vertical-layout>
-        `;
+<vaadin-vertical-layout style="width: 100%; height: 100%;" id="vaadinVerticalLayout">
+ <vaadin-horizontal-layout style="width:100%; height: 20%" class="background">
+  <!-- Tu ide mood ili ask me a question-->
+ </vaadin-horizontal-layout>
+ <vaadin-vertical-layout style="height: 100%; width:100%;" class="background">
+  <vaadin-rich-text-editor class="editor" html-value="<p><br></p>"></vaadin-rich-text-editor>
+  <vaadin-button class="btn">
+    Save 
+  </vaadin-button>
+ </vaadin-vertical-layout>
+</vaadin-vertical-layout>
+`;
     }
 
     static get is() {
--- src/main/java/hr/unipu/diary/views/texteditor/TexteditorView.java
+++ src/main/java/hr/unipu/diary/views/texteditor/TexteditorView.java
@@ -1,5 +1,7 @@
 package hr.unipu.diary.views.texteditor;
 
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.dom.Element;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
@@ -20,11 +22,14 @@ import hr.unipu.diary.views.main.MainView;
 @Tag("texteditor-view")
 public class TexteditorView extends PolymerTemplate<TexteditorView.TexteditorViewModel> {
 
+    @Id("vaadinVerticalLayout")
+    private Element vaadinVerticalLayout;
+
     /**
      * Creates a new TexteditorView.
      */
     public TexteditorView() {
-        // You can initialise any data required for the connected UI components here.
+        vaadinVerticalLayout.getStyle().set("background-image", "url('images/editor.png')");
     }
 
     /**
