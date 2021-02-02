3cf585d1707d7fd7c31ca0f0a58ee298cfb81f43
nikki <6318495+echrs@users.noreply.github.com>
2021-02-02 02:29:23 +0100

    text for empty stats

--- frontend/src/views/stats/stats-view.js
+++ frontend/src/views/stats/stats-view.js
@@ -35,20 +35,23 @@ class StatsView extends PolymerElement {
 <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;" id="vaadinHorizontalLayout">
  <vaadin-vertical-layout theme="spacing" style="width: 75%; height: 100%;" id="vaadinVerticalLayout"></vaadin-vertical-layout>
  <vaadin-vertical-layout style="width: 100%; height: 100%;">
- <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;">
- <div class="center-top">
-  <vaadin-chart title="Moods by Dates" categories="" id="moodChart">
-   <vaadin-chart-series title="Moods" unit="Worst to Best [1-5]" values=""></vaadin-chart-series>
-  </vaadin-chart>
- </div>
-</vaadin-horizontal-layout>
-<vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;">
- <div class="center-bot">
-  <vaadin-chart type="pie" title="Moods Pie Chart" tooltip="" id="pieChart">
-   <vaadin-chart-series title="Entries" values=""></vaadin-chart-series>
-  </vaadin-chart>
- </div>
-</vaadin-horizontal-layout>
+  <div id="isNone">
+    Div 
+  </div>
+  <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;">
+   <div class="center-top">
+    <vaadin-chart title="Moods by Dates" categories="" id="moodChart">
+     <vaadin-chart-series title="Moods" unit="Worst to Best [1-5]" values=""></vaadin-chart-series>
+    </vaadin-chart>
+   </div>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing" style="width: 100%; height: 100%;">
+   <div class="center-bot">
+    <vaadin-chart type="pie" title="Moods Pie Chart" tooltip="" id="pieChart">
+     <vaadin-chart-series title="Entries" values=""></vaadin-chart-series>
+    </vaadin-chart>
+   </div>
+  </vaadin-horizontal-layout>
  </vaadin-vertical-layout>
  <vaadin-vertical-layout theme="spacing" style="width: 75%; height: 100%;" id="vaadinVerticalLayout1"></vaadin-vertical-layout>
 </vaadin-horizontal-layout>
